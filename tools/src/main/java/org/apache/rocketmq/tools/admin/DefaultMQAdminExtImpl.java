a810c12f10273a5061271dbbef29286cfd5fc466
zjluo <zjluo@iflytek.com>
2019-11-07 17:09:04 +0800

    去除消息消费情况跟踪的时候比对存储broker的ip的逻辑，因为从namesrv取到的是域名，消息中存储的是ip

--- tools/src/main/java/org/apache/rocketmq/tools/admin/DefaultMQAdminExtImpl.java
+++ tools/src/main/java/org/apache/rocketmq/tools/admin/DefaultMQAdminExtImpl.java
@@ -842,11 +842,11 @@ public class DefaultMQAdminExtImpl implements MQAdminExt, MQAdminExtInner {
                 BrokerData brokerData = ci.getBrokerAddrTable().get(mq.getBrokerName());
                 if (brokerData != null) {
                     String addr = brokerData.getBrokerAddrs().get(MixAll.MASTER_ID);
-                    if (addr.equals(RemotingUtil.socketAddress2String(msg.getStoreHost()))) {
+//                    if (addr.equals(RemotingUtil.socketAddress2String(msg.getStoreHost()))) {
                         if (next.getValue().getConsumerOffset() > msg.getQueueOffset()) {
                             return true;
                         }
-                    }
+//                    }
                 }
             }
         }
